039ffa906e2ec99349073b2a78a4c3abf3bd08c7
Abhishek0 <Abhishek0@linux.com>
2026-01-27 10:51:28 +0530

    change helloworld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,6 +2,10 @@
 # This Phython program will print Hellow World...
 print("Hello World Demo......\n")
 print("Hello World Demo......\n")
+print("Hello World Demo......\n")
+print("Hello World Demo......\n")
+print("Hello World Demo......\n")
+print("Hello World Demo......\n")
 
 
 
